f49309ab2153cc9ad26171e353c48b3b06f296d2
ashpurple <ashpurple9@gmail.com>
2022-01-04 20:57:29 +0900

    Create: SDS 자료구조

---
+++ Samsung SDS/BOJ_11279_최대힙.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main (){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N, num;
+    cin >> N;
+
+    priority_queue<int> pq;
+    
+    for(int i = 0; i < N; i++){
+        cin >> num;
+        if(num == 0){
+            if(pq.empty()){
+                cout << 0 << '\n';
+            }
+            else{
+                cout << pq.top() << '\n';
+                pq.pop();
+            }
+        }
+        else{
+            pq.push(num);
+        }
+    }
+}
+
