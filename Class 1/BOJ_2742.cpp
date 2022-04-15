dcb5c2ce5754ebd4f86d31837a94c6cf0ea44ede
DESKTOP-43FIQJP\dkstp <ashpurple9@gmail.com>
2022-04-16 01:00:00 +0900

    Create: BOJ_2742 기찍N

---
+++ Class 1/BOJ_2742.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+    for(int i = N; 0 < i; i--) cout << i << '\n';
+}
