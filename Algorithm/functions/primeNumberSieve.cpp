08d01de25906e433eae39c7b83c146a559841a43
DESKTOP-43FIQJP\dkstp <ashpurple9@gmail.com>
2022-05-12 15:30:23 +0900

    Update: Function - PrimeNumberSieve

--- Algorithm/functions/primeNumberSieve.cpp
+++ Algorithm/functions/primeNumberSieve.cpp
@@ -2,21 +2,20 @@
 using namespace std;
 
 int prime[1000];     
-bool check[1001] = {false};      
-int theNumOfPrimes = 0;
+bool check[1001];      
+int theNumOfPrimes;
 int N;
 
 void primeNumberSieve(){
     for (int i = 2; i <= N; ++i){
-            if (check[i] == false){
+            if (!check[i]){
                 prime[theNumOfPrimes] = i;
                 theNumOfPrimes++;
             }
     
             for (int j = i * i; j <= N; j += i){
-                if (check[j] == false){
-                    check[j] = true;
-                }
+                // i * i Integer 범위 초과 조심
+                if (!check[j]) check[j] = true;
             }
         }
 }
